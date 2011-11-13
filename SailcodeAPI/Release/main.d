9ccaa64b5077e4dfd628c167430aca04d3bc470e
allgood38 <allgood38@allgood38-MT3418.(none)>
2011-11-13 13:26:39 -0500

    Set up an eclipse project for Sailcode API

---
+++ SailcodeAPI/Release/main.d
@@ -0,0 +1,25 @@
+main.d main.o: ../main.cpp \
+  /home/allgood38/git/newmast/ArduinoCore/WProgram.h \
+  /home/allgood38/git/newmast/ArduinoCore/wiring.h \
+  /home/allgood38/git/newmast/ArduinoCore/binary.h \
+  /home/allgood38/git/newmast/ArduinoCore/WCharacter.h \
+  /home/allgood38/git/newmast/ArduinoCore/WString.h \
+  /home/allgood38/git/newmast/ArduinoCore/HardwareSerial.h \
+  /home/allgood38/git/newmast/ArduinoCore/Stream.h \
+  /home/allgood38/git/newmast/ArduinoCore/Print.h
+
+/home/allgood38/git/newmast/ArduinoCore/WProgram.h:
+
+/home/allgood38/git/newmast/ArduinoCore/wiring.h:
+
+/home/allgood38/git/newmast/ArduinoCore/binary.h:
+
+/home/allgood38/git/newmast/ArduinoCore/WCharacter.h:
+
+/home/allgood38/git/newmast/ArduinoCore/WString.h:
+
+/home/allgood38/git/newmast/ArduinoCore/HardwareSerial.h:
+
+/home/allgood38/git/newmast/ArduinoCore/Stream.h:
+
+/home/allgood38/git/newmast/ArduinoCore/Print.h:
